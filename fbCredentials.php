89a1b366665790c0151510444231226aa9eb2974
mehulkaklotar <mehul.kaklotar@gmail.com>
2013-12-28 00:23:47 +0530

    Credentials for Facebook

---
+++ fbCredentials.php
@@ -0,0 +1,6 @@
+<?php 
+//Set Facebook App id and App Secret
+//Create our Application instance (replace this with your appId and secret).
+ $AppId='737631022923182';
+ $AppSecret='6c533a4b735995f6889d4700b575e0ad';
+?>
